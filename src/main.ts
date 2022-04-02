c730bd4aa5256531a033adfe11dfde075c23c8ca
Abdelrahman AlShakhshir <abood1shakhshir@gmail.com>
2022-04-02 21:53:21 +0400

    fix swagger titles

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,10 @@ async function bootstrap() {
   });
 
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
+  .setTitle('ConCruise api')
+  .setDescription('find customers and drivers easily')
   .setVersion('1.0')
-  .addTag('cats')
+  .addTag('ConCruise')
   .build();
 
 
